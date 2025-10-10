4e4be10232932821db3ff71ebcfae96f9a743c0e
Faizan Ali <faizanali3n@gmail.com>
2025-10-10 16:52:27 +0500

    Type conversion

--- string_method.py
+++ string_method.py
@@ -3,3 +3,7 @@ course = "python programming"
 print(course.upper())
 print(course.lower())
 print(course.strip())
+print(course.strip())
+print(course.find("pro"))
+print(course.replace("p", "j"))
+
